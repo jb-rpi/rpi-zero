8c5fcf2c0b71dbaf98522a201198a27662498fd2
jb-rpi <34139892+jb-rpi@users.noreply.github.com>
2022-02-26 15:03:51 +0100

    Create capture.py

---
+++ camera/capture.py
@@ -0,0 +1,12 @@
+
+#taken from picamera 
+
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera()
+
+camera.start_preview()
+sleep(5)
+camera.capture('/home/pi/ftp/image2.jpg')
+camera.stop_preview()
